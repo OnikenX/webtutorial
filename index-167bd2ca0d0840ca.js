624d3a0631ff77d34e13139a21a6a74fa595fce9
OnikenX <OnikenX@pm.me>
2021-10-27 04:01:21 +0100

    update gh-pages

--- index-167bd2ca0d0840ca.js
+++ index-167bd2ca0d0840ca.js
@@ -208,7 +208,7 @@ function addBorrowedObject(obj) {
 }
 function __wbg_adapter_16(arg0, arg1, arg2) {
     try {
-        wasm._dyn_core__ops__function__FnMut___A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h084e039ed257f724(arg0, arg1, addBorrowedObject(arg2));
+        wasm._dyn_core__ops__function__FnMut___A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h5f5a27a4cc9df568(arg0, arg1, addBorrowedObject(arg2));
     } finally {
         heap[stack_pointer++] = undefined;
     }
@@ -259,7 +259,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-c09ccfad49cccc8d_bg.wasm', import.meta.url);
+        input = new URL('index-167bd2ca0d0840ca_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
@@ -489,7 +489,7 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper309 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper310 = function(arg0, arg1, arg2) {
         var ret = makeMutClosure(arg0, arg1, 129, __wbg_adapter_16);
         return addHeapObject(ret);
     };
